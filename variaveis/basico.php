84d891628bed92581ce244c40bc3b63073b73a4c
GuilhermeKochepki <gui.kochepki@gmail.com>
2021-08-26 11:17:34 -0300

    Desenvolvendo o básico sobre as variáveis

--- variaveis/basico.php
+++ variaveis/basico.php
@@ -4,3 +4,28 @@
 $numeroA = 3;
 echo $numeroA, '<br>';
 var_dump($numeroA);
+
+echo '<br>';
+$a = 3;
+$b = 16;
+$soma = $a + $b;
+echo $soma;
+
+echo '<br>';
+echo isset($soma);
+
+echo '<br>';
+unset($soma);
+echo isset($soma);
+
+echo '<br>';
+unset($soma);
+echo !isset($soma);
+echo '<br>';
+var_dump($soma);
+
+$variavel = 10;
+echo '<br>' . $variavel;
+
+$variavel = "Agora sou uma string";
+echo '<br>' . $variavel;
